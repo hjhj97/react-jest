fe4a15860413520e8725539036245d715da930b4
JuHeonHa <hjhjaueon97@naver.com>
2023-06-24 14:37:30 +0900

    test : counter 테스트코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterEl = screen.getByTestId("counter");
+
+  expect(counterEl).toHaveTextContent(0);
 });
